35244c187af848576cdf2c6bf3e41df50ce7e48d
stiegerc <stiegerc@gmail.com>
2022-02-16 00:03:10 +0100

    removed some old crap from the main function

--- src/chrigizip.cc
+++ src/chrigizip.cc
@@ -53,55 +53,5 @@ int main(const int argc, char** argv) {
 		os << "ERROR: " << e.what() << "\n";
 	}
 
-
-/*
-	{
-		ctree CT("test");
-		CT.compress("test","gaggi.chrigi");
-	}
-	{
-		std::ifstream ifile("gaggi.chrigi", std::ios::binary);
-		if (!ifile)
-			throw(std::invalid_argument("bad file"));
-
-		ctree CT(ifile);
-		std::cout << "\n\n" << CT.filename() << "\n\n";
-	}
-*/
-
-//	chrigi::compress("testfiles/test","test.chrigi");
-//	chrigi::compress("testfiles/kenny.jpeg","kenny.chrigi",2,std::cout);
-//	chrigi::compress("testfiles/test2","test2.chrigi",2,std::cout);
-//	chrigi::compress("testfiles/test","test.chrigi",2,std::cout);
-//	chrigi::compress("test2.chrigi","test2.chrigi.chrigi");
-//	chrigi::compress("test2.chrigi.chrigi","test2.chrigi.chrigi.chrigi");
-//	chrigi::decompress("test2.chrigi.chrigi.chrigi");
-//	chrigi::decompress("test2.chrigi.chrigi");
-//	chrigi::decompress("test2.chrigi");
-
-//	ifstream__ ifile("test");
-//	compression_tree T(ifile, compression_tree::read_mode::from_file);
-
-
-
-/*
-	{
-		ifstream__ ifile("test");
-		char* blob = new char [ifile.Nbytes_left()];
-		std::cout << "\n\n" << ifile.Nbytes_left() << "\n\n";
-		ifile.read(blob,ifile.Nbytes_left());
-		std::cout << "\n\n" << ifile.Nbytes_left() << "\n\n";
-		delete[] blob;
-
-		ifstream__ ifile2("test");
-		std::cout << "\n\n" << ifile2.Nbytes_left() << "\n\n";
-	}
-	{
-		ifstream__ ifile("test");
-		std::cout << "\n\n" << ifile.Nbytes_left() << "\n\n";
-	}
-*/
-
-
 	return 0;
 }
